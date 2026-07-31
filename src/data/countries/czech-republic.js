8fe670230ed14b3987e17e886357a49717bb76ec
Nicholas Moffett <152448581+moff05@users.noreply.github.com>
2026-07-31 08:28:47 -0500

    data: Czech Republic

--- src/data/countries/czech-republic.js
+++ src/data/countries/czech-republic.js
@@ -100,7 +100,7 @@ export const czechRepublic = {
       }
     },
     "1938": {
-      "result": "Quarter-final",
+      "result": "Round of 16",
       "headline": "Two Games, One Opponent",
       "homeColors": "white shirt, red shorts, blue socks",
       "awayColors": null,
@@ -451,7 +451,7 @@ export const czechRepublic = {
       }
     },
     "1990": {
-      "result": "Quarter-final",
+      "result": "Round of 16",
       "headline": "Skuhravý's Five-Goal Spring",
       "homeColors": "white shirt, red shorts, blue socks",
       "awayColors": null,
