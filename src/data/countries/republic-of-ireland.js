20e99df6bf44b14a72806bd99c2c9bd98cc1dce8
Nicholas Moffett <152448581+moff05@users.noreply.github.com>
2026-07-31 08:28:48 -0500

    data: Republic of Ireland

--- src/data/countries/republic-of-ireland.js
+++ src/data/countries/republic-of-ireland.js
@@ -31,7 +31,7 @@ export const republicOfIreland = {
   ],
   "kits": {
     "1990": {
-      "result": "Quarter-final",
+      "result": "Round of 16",
       "headline": "Unbeaten Into the Quarters",
       "homeColors": "emerald green shirt with white and orange wrap-over V-neck trim (Adidas), white shorts, green socks",
       "awayColors": "white shirt with green trim (Adidas)",
