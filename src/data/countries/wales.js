eb0afd235af0012916b0b34b448c64834bcb029c
Nicholas Moffett <152448581+moff05@users.noreply.github.com>
2026-07-31 08:28:49 -0500

    data: Wales

--- src/data/countries/wales.js
+++ src/data/countries/wales.js
@@ -30,7 +30,7 @@ export const wales = {
   ],
   "kits": {
     "1958": {
-      "result": "Quarter-final",
+      "result": "Group Stage",
       "headline": "Pelé Ends the Dream",
       "homeColors": "red shirt with white shorts, reintroduced for the 1958 tournament",
       "awayColors": "yellow and green change kit, used for the first time at a World Cup",
