d01c7dedec89a0031348875c9d8ca873354b8758
Nicholas Moffett <152448581+moff05@users.noreply.github.com>
2026-08-26 14:42:25 -0400

    data: United States

--- src/data/countries/usa.js
+++ src/data/countries/usa.js
@@ -40,7 +40,7 @@ export const usa = {
   ],
   "kits": {
     "1930": {
-      "result": "Group Stage",
+      "result": "Semi-final",
       "headline": "Patenaude's Historic Hat-Trick",
       "homeColors": "white shirt, blue shorts, white socks",
       "awayColors": null,
@@ -363,7 +363,7 @@ export const usa = {
       }
     },
     "2002": {
-      "result": "Round of 16",
+      "result": "Quarter-final",
       "headline": "Golden Generation's Quarter-Final",
       "homeColors": "white shirt, navy blue shorts, white socks (Nike)",
       "awayColors": null,
