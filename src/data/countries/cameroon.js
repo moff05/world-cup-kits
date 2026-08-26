7387ce699a687e2a8c610118f0b8b9d3e5f35e60
Nicholas Moffett <152448581+moff05@users.noreply.github.com>
2026-08-26 14:42:24 -0400

    data: Cameroon

--- src/data/countries/cameroon.js
+++ src/data/countries/cameroon.js
@@ -91,7 +91,7 @@ export const cameroon = {
       }
     },
     "1990": {
-      "result": "Round of 16",
+      "result": "Quarter-final",
       "headline": "Milla's Dance, Argentina Stunned",
       "homeColors": "green shirt, red shorts, yellow socks",
       "awayColors": null,
